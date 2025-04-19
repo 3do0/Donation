03d2cf6347b782975ece8a97305b33f2d572c4f3
Your Name <your-email@example.com>
2025-04-19 23:59:56 +0300

    livewire2

--- config/livewire.php
+++ config/livewire.php
@@ -66,16 +66,17 @@ return [
     'temporary_file_upload' => [
         'disk' => 'public',        // Example: 'local', 's3'              | Default: 'default'
         'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => 'livewire-tmp',
-        'middleware' => ['web'], // ← هذا أهم شيء لإصلاح الخطأ 401 عند الرفع
-        'preview_mimes' => [
+        'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
+        'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
+        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
             'jpg', 'jpeg', 'mpga', 'webp', 'wma',
         ],
-        'max_upload_time' => 5,
-        'cleanup' => true,
+        'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
+        'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
     ],
+
     /*
     |---------------------------------------------------------------------------
     | Render On Redirect
